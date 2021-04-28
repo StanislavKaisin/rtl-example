014b241674337b686e817f2e2525006d48b244e7
StanislavKaisin <8050361@gmail.com>
2021-04-28 22:40:43 +0300

    tested rendered component with snapshot

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
+test("renders a component", () => {
   // const linkElement = screen.getByText(/learn react/i);
   // expect(linkElement).toBeInTheDocument();
-  screen.debug();
+  // screen.debug();
+  const { asFragment } = render(<App />);
+  expect(asFragment(<App />)).toMatchSnapshot();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+interface ISearch {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  children: any;
+}
+
+const Search = ({ value, onChange, children }: ISearch) => {
+  return (
+    <div>
+      <label htmlFor="search">{children}</label>
+      <input id="search" type="text" value={value} onChange={onChange} />
+    </div>
+  );
+};
+
 function App() {
+  const [search, setSearch] = useState("");
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React test
-        </a>
-      </header>
+      <Search value={search} onChange={handleChange}>
+        Search:
+      </Search>
+      <p>Searches for {search ? search : "..."}</p>
     </div>
   );
 }
