30fcfb7c45640ca11e04827a43654457f1cc7011
StanislavKaisin <8050361@gmail.com>
2021-05-21 21:34:11 +0300

    fixed router tests

--- src/ReactRouter.test.tsx
+++ src/ReactRouter.test.tsx
@@ -45,7 +45,7 @@ const RouterComponent = () => {
   );
 };
 
-const renderRouter = (
+const renderWithRouter = (
   component,
   {
     route = "/",
@@ -60,12 +60,13 @@ const renderRouter = (
 
 describe("React Router", () => {
   it("should render the home page", () => {
-    const history = createMemoryHistory();
-    const { container, getByTestId } = render(
-      <Router history={history}>
-        <RouterComponent />
-      </Router>
-    );
+    // const history = createMemoryHistory();
+    // const { container, getByTestId } = render(
+    //   <Router history={history}>
+    //     <RouterComponent />
+    //   </Router>
+    // );
+    const { container, getByTestId } = renderWithRouter(<RouterComponent />);
     const navbar = getByTestId("navbar");
     const link = getByTestId("home-link");
     expect(container.innerHTML).toMatch("Home page");
@@ -73,25 +74,45 @@ describe("React Router", () => {
   });
 
   it("should navigate to page", () => {
-    const history = createMemoryHistory();
-    const { container, getByTestId } = render(
-      <Router history={history}>
-        <RouterComponent />
-      </Router>
-    );
+    // const history = createMemoryHistory();
+    // const { container, getByTestId } = render(
+    //   <Router history={history}>
+    //     <RouterComponent />
+    //   </Router>
+    // );
+    const { container, getByTestId } = renderWithRouter(<RouterComponent />);
     fireEvent.click(getByTestId("contact-link"));
     expect(container.innerHTML).toMatch(NAME);
   });
 
   it("should navigate to error page if route is wrong", () => {
-    const history = createMemoryHistory();
-    history.push("wrong-route");
-    const { container, getByTestId } = render(
-      <Router history={history}>
-        <RouterComponent />
-      </Router>
-    );
+    // const history = createMemoryHistory();
+    // history.push("wrong-route");
+    // const { container, getByTestId } = render(
+    //   <Router history={history}>
+    //     <RouterComponent />
+    //   </Router>
+    // );
+    const { container } = renderWithRouter(<RouterComponent />, {
+      route: "wrong-route",
+    });
     expect(container.innerHTML).toMatch("404 Error");
   });
+
+  it("should render component that use withRouter", () => {
+    // const history = createMemoryHistory();
+    // const route = "/some-route";
+    // history.push(route);
+
+    // const { container, getByTestId } = render(
+    //   <Router history={history}>
+    //     <RouterComponent />
+    //   </Router>
+    // );
+    const route = "/some-route";
+    const { getByTestId } = renderWithRouter(<RouterComponent />, {
+      route: route,
+    });
+    expect(getByTestId("location-display")).toHaveTextContent(route);
+  });
 });
-o;
