61bfa94b7d16868f7032f22d5f18ba3af6754b8c
StanislavKaisin <8050361@gmail.com>
2021-05-24 21:04:08 +0300

    tested redux and useReducer hook

---
+++ src/redux/TestRedux.test.tsx
@@ -0,0 +1,40 @@
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { reducer } from "./reducer";
+import TestRedux from "./TestRedux";
+
+const renderWithRedux = (
+  component,
+  { initialState, store = createStore(reducer, initialState) } = {}
+) => {
+  return {
+    ...render(<Provider store={store}>{component}</Provider>),
+    store,
+  };
+};
+
+describe("Redux testing", () => {
+  it("checks initial state is equal to 0", () => {
+    const { getByRole } = renderWithRedux(<TestRedux />);
+    expect(getByRole("heading")).toHaveTextContent("0");
+  });
+
+  it("increments the counter throurg redux", () => {
+    const { getByRole, getByText } = renderWithRedux(<TestRedux />, {
+      initialState: { count: 5 },
+    });
+    userEvent.click(getByText("+1"));
+    expect(getByRole("heading")).toHaveTextContent("6");
+  });
+
+  it("decrements the counter throurg redux", () => {
+    const { getByRole, getByText } = renderWithRedux(<TestRedux />, {
+      initialState: { count: 100 },
+    });
+    userEvent.click(getByText("-1"));
+    expect(getByRole("heading")).toHaveTextContent("99");
+  });
+});
